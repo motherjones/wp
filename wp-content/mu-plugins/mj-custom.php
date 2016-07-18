d101219da78c95e816aa9c96ab2d0f17b474345a
Ben Breedlove <benbreedlove@gmail.com>
2016-07-18 16:38:53 -0400

    new format for adding fields required because of how the hooks work

--- wp-content/mu-plugins/mj-custom.php
+++ wp-content/mu-plugins/mj-custom.php
@@ -14,24 +14,28 @@ require_once('mj_custom/motherjones-content-types.php');
 require_once('fieldmanager/fieldmanager.php');
 
 
+$master_image = new Fieldmanager_Group( array(
+  'name' => 'master_image',
+  'children' => array(
+    'master_image' => new Fieldmanager_Media( 'Image' ),
+    'master_image_byline' => new Fieldmanager_Textfield( 'Art Byline' ),
+    'master_image_caption' => new Fieldmanager_Textfield( 'Caption' ),
+    'master_image_suppress' => new Fieldmanager_Checkbox( array(
+      'label' => 'Suppress Master Image',
+      'unchecked_value' => 'Show Master Image',
+      'checked_value' => 'Hide Master Image',
+    ) )
+  )
+) );
+
 add_action( 'fm_post_mj_blog_post', function() {
-  $fm = new Fieldmanager_Group( array(
-    'name' => 'master_image',
-    'children' => array(
-      'master_image' => new Fieldmanager_Media( 'Image' ),
-      'master_image_byline' => new Fieldmanager_Textfield( 'Art Byline' ),
-      'master_image_caption' => new Fieldmanager_Textfield( 'Caption' ),
-      'master_image_suppress' => new Fieldmanager_Checkbox( array(
-        'label' => 'Suppress Master Image',
-        'unchecked_value' => 'Show Master Image',
-        'checked_value' => 'Hide Master Image',
-      ) )
-    )
-  ) );
-  $fm->add_meta_box( 'Master Image', 'mj_article' );
-  $fm->add_meta_box( 'Master Image', 'mj_blog_post' );
-  $fm->add_meta_box( 'Master Image', 'mj_full_width' );
-  $fm->add_meta_box( 'Master Image', 'post' );
+  $master_image->add_meta_box( 'Master Image', 'mj_blog_post' );
+} );
+add_action( 'fm_post_mj_article', function() {
+  $master_image->add_meta_box( 'Master Image', 'mj_article' );
+} );
+add_action( 'fm_post_mj_full_width', function() {
+  $master_image->add_meta_box( 'Master Image', 'mj_full_width' );
 } );
 
 ?>
