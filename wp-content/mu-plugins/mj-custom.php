de97e615b4e49008d0d4ad627645c45d728a5a95
ben <benbreedlove@gmail.com>
2016-07-21 15:48:28 -0400

    oh maybe multi things need ot be in a group

--- wp-content/mu-plugins/mj-custom.php
+++ wp-content/mu-plugins/mj-custom.php
@@ -116,14 +116,19 @@ function byline() {
 }
 
 function related() {
-	return new Fieldmanager_Autocomplete( array(
+	return new Fieldmanager_Group( array(
 		'label'      => 'Related Articles',
 		'limit'      => 0,
-//		'sortable'   => true,
-		'add_more_label' => 'Add another',
-		'datasource' => new Fieldmanager_Datasource_Post( array(
-			'query_args' => array( 'post_type' => 'mj_article,mj_full_width,mj_blog_post' )
-		) ),
+		'sortable'   => true,
+		'add_more_label' => 'Add another article',
+    'children'   => array(
+      'related_article' => Fieldmanager_Autocomplete( array(
+        'label' => 'Related Article',
+        'datasource' => new Fieldmanager_Datasource_Post( array(
+          'query_args' => array( 'post_type' => 'mj_article,mj_full_width,mj_blog_post' )
+        ) ),
+      ) ),
+    )
 	) );
 
 }
