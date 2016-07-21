f8e95ef8301c483ae534c67b5173776343c0d048
ben <benbreedlove@gmail.com>
2016-07-21 15:43:34 -0400

    is it the limit?

--- wp-content/mu-plugins/mj-custom.php
+++ wp-content/mu-plugins/mj-custom.php
@@ -118,6 +118,9 @@ function byline() {
 function related() {
 	return new Fieldmanager_Autocomplete( array(
 		'label'      => 'Related Articles',
+		'limit'      => 0,
+//		'sortable'   => true,
+		'add_more_label' => 'Add another',
 		'datasource' => new Fieldmanager_Datasource_Post( array(
 			'query_args' => array( 'post_type' => 'mj_article,mj_full_width,mj_blog_post' )
 		) ),
