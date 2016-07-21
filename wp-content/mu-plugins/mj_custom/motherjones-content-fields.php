c9b4ced5439eb62884ef93ecb63b537aede659ae
ben <benbreedlove@gmail.com>
2016-07-21 17:22:02 -0400

    um wat

--- wp-content/mu-plugins/mj_custom/motherjones-content-fields.php
+++ wp-content/mu-plugins/mj_custom/motherjones-content-fields.php
@@ -10,184 +10,182 @@
 */
 require_once('../fieldmanager/fieldmanager.php');
 
-if ( !class_exists( 'MJ_Custom_Fields' ) ) :
+if ( !class_exists( 'MJ_Custom_Fields' ) ) {
 
-class MJ_Custom_Fields {
-  /* Article type fields (also blogposts, full widths) */
-  //fullwidth only!
-  public function title_image() { 
-    return new Fieldmanager_Group( array(
-      'name' => 'full_width_title_image',
-      'children' => array(
-        'title_image' => new Fieldmanager_Media( 'Title Image' ),
-        'title_image_byline' => new Fieldmanager_TextField( 'Title Image Byline' ),
-      ),
-    ) );
-  }
-  //end fullwidth only!
+  class MJ_Custom_Fields {
+    /* Article type fields (also blogposts, full widths) */
+    public function title_image() { 
+      return new Fieldmanager_Group( array(
+        'name' => 'full_width_title_image',
+        'children' => array(
+          'title_image' => new Fieldmanager_Media( 'Title Image' ),
+          'title_image_byline' => new Fieldmanager_TextField( 'Title Image Byline' ),
+        ),
+      ) );
+    }
 
-  public function dek() {
-    return new Fieldmanager_TextField( array(
-      'name' => 'dek',
-    ) );
-  }
+    public function dek() {
+      return new Fieldmanager_TextField( array(
+        'name' => 'dek',
+      ) );
+    }
 
-  public function social() {
-    return new Fieldmanager_Group( array(
-      'name' => 'social',
-      'children' => array(
-        'social_title' => new Fieldmanager_TextField( 'Social Title' ),
-        'social_dek' => new Fieldmanager_TextField( 'Social Dek' ),
-        'standout' => new Fieldmanager_Checkbox( array(
-          'label' => 'Mark as Google News Standout',
-        ) ),
-        'fb_instant_exclude' => new Fieldmanager_Checkbox( array(
-          'label' => 'Exclude from Facebook Instant',
-        ) ),
-      )
-    ) );
-  }
-
-  public function alt() {
-    return new Fieldmanager_Group( array(
-      'name' => 'alt',
-      'description' => "How this'll look on the homepage",
-      'children' => array(
-        'alt_title' => new Fieldmanager_TextField( 'Alt Title' ),
-        'alt_dek' => new Fieldmanager_TextField( 'Alt Dek' ),
-      )
-    ) );
-  }
-
-  public function master_image() {
-    return new Fieldmanager_Group( array(
-      'name' => 'master_image',
-      'children' => array(
-        'master_image' => new Fieldmanager_Media( 'Image' ),
-        'master_image_byline' => new Fieldmanager_TextField( 'Art Byline' ),
-        'master_image_caption' => new Fieldmanager_TextField( 'Caption' ),
-        'master_image_suppress' => new Fieldmanager_Checkbox( array(
-          'label' => 'Suppress Master Image',
-          'unchecked_value' => 'Show Master Image',
-          'checked_value' => 'Hide Master Image',
-        ) )
-      )
-    ) );
-  }
-
-  public function body() { 
-    return new Fieldmanager_TextArea( array(
-      'name' => 'body'
-    ) );
-  }
-
-  //TAXONOMIES!!!! FFFFFFFFFUUUUUUUUUUUU
-
-  //scheduling?
-
-
-  //file attachments?
-
-  public function css_js() {
-    return new Fieldmanager_Group( array(
-      'name' => 'css_js',
-      'children' => array(
-        'css' => new Fieldmanager_TextArea( 'CSS' ),
-        'js' => new Fieldmanager_TextArea( 'Javascript' ),
-      )
-    ) );
-  }
-
-  //dumb thing we have to do because autocompletes can't happen too early
-  public function byline() {
-    return new Fieldmanager_Group( array( 
-      'name' => 'byline',
-      'children' => array(
-        'authors' => new Fieldmanager_Autocomplete( "Author", array(
-          'limit'      => 0,
-          'sortable'   => true,
-          'add_more_label' => 'Add another author',
-          'datasource' => new Fieldmanager_Datasource_Post( array(
-            'query_args' => array( 
-              'post_type' => 'mj_author',
-              'post_status' => 'publish'
-            )
+    public function social() {
+      return new Fieldmanager_Group( array(
+        'name' => 'social',
+        'children' => array(
+          'social_title' => new Fieldmanager_TextField( 'Social Title' ),
+          'social_dek' => new Fieldmanager_TextField( 'Social Dek' ),
+          'standout' => new Fieldmanager_Checkbox( array(
+            'label' => 'Mark as Google News Standout',
           ) ),
-        ) ),
-        'override' => new Fieldmanager_TextField( 'Byline Override' )
-      )
-    ) );
-  }
-
-  public function related() {
-    return new Fieldmanager_Group( array( 
-      'name' => 'related_articles',
-      'children' => array(
-        'relateds' => new Fieldmanager_Autocomplete( "Article", array(
-          'limit'      => 0,
-          'sortable'   => true,
-          'add_more_label' => 'Add another article',
-          'datasource' => new Fieldmanager_Datasource_Post( array(
-            'query_args' => array( 
-              'post_type' => array('mj_blog_post', 'mj_article', 'mj_full_width'),
-              'post_status' => 'publish'
-            )
+          'fb_instant_exclude' => new Fieldmanager_Checkbox( array(
+            'label' => 'Exclude from Facebook Instant',
           ) ),
-        ) )
-      )
-    ) );
-  }
-
-  /* end article type fields */
-
-  /* begin author type fields */
-
-  public function position() {
-    return new Fieldmanager_TextField( array(
-      'name' => 'position',
-    ) );
-  }
-
-  public function image() {
-    return new Fieldmanager_Media( array( 
-      'name' => 'image'
-    ) );
-  }
-
-  public function long_bio() {
-    return new Fieldmanager_TextArea( array(
-      'name' => 'long_bio'
-    ) );
-  }
-
-  public function short_bio() {
-    return new Fieldmanager_TextArea( array(
-      'name' => 'short_bio'
-    ) );
-  }
-
-  public function twitter() {
-    return new Fieldmanager_TextField( array(
-      'name' => 'twitter',
-    ) );
-  }
-
-
-  /* end author type fields */
-
-  /* begin user type fields */
-  public function author() {
-    return new Fieldmanager_Autocomplete( array(
-      'name'      => 'author',
-      'datasource' => new Fieldmanager_Datasource_Post( array(
-        'query_args' => array( 
-          'post_type' => 'mj_author',
-          'post_status' => 'publish'
         )
-      ) ),
-    ) );
-  }
-  /* end user type fields */
-}
+      ) );
+    }
 
-endif;
+    public function alt() {
+      return new Fieldmanager_Group( array(
+        'name' => 'alt',
+        'description' => "How this'll look on the homepage",
+        'children' => array(
+          'alt_title' => new Fieldmanager_TextField( 'Alt Title' ),
+          'alt_dek' => new Fieldmanager_TextField( 'Alt Dek' ),
+        )
+      ) );
+    }
+
+    public function master_image() {
+      return new Fieldmanager_Group( array(
+        'name' => 'master_image',
+        'children' => array(
+          'master_image' => new Fieldmanager_Media( 'Image' ),
+          'master_image_byline' => new Fieldmanager_TextField( 'Art Byline' ),
+          'master_image_caption' => new Fieldmanager_TextField( 'Caption' ),
+          'master_image_suppress' => new Fieldmanager_Checkbox( array(
+            'label' => 'Suppress Master Image',
+            'unchecked_value' => 'Show Master Image',
+            'checked_value' => 'Hide Master Image',
+          ) )
+        )
+      ) );
+    }
+
+    public function body() { 
+      return new Fieldmanager_TextArea( array(
+        'name' => 'body'
+      ) );
+    }
+
+    //TAXONOMIES!!!! FFFFFFFFFUUUUUUUUUUUU
+
+    //scheduling?
+
+
+    //file attachments?
+
+    public function css_js() {
+      return new Fieldmanager_Group( array(
+        'name' => 'css_js',
+        'children' => array(
+          'css' => new Fieldmanager_TextArea( 'CSS' ),
+          'js' => new Fieldmanager_TextArea( 'Javascript' ),
+        )
+      ) );
+    }
+
+    //dumb thing we have to do because autocompletes can't happen too early
+    public function byline() {
+      return new Fieldmanager_Group( array( 
+        'name' => 'byline',
+        'children' => array(
+          'authors' => new Fieldmanager_Autocomplete( "Author", array(
+            'limit'      => 0,
+            'sortable'   => true,
+            'add_more_label' => 'Add another author',
+            'datasource' => new Fieldmanager_Datasource_Post( array(
+              'query_args' => array( 
+                'post_type' => 'mj_author',
+                'post_status' => 'publish'
+              )
+            ) ),
+          ) ),
+          'override' => new Fieldmanager_TextField( 'Byline Override' )
+        )
+      ) );
+    }
+
+    public function related() {
+      return new Fieldmanager_Group( array( 
+        'name' => 'related_articles',
+        'children' => array(
+          'relateds' => new Fieldmanager_Autocomplete( "Article", array(
+            'limit'      => 0,
+            'sortable'   => true,
+            'add_more_label' => 'Add another article',
+            'datasource' => new Fieldmanager_Datasource_Post( array(
+              'query_args' => array( 
+                'post_type' => array('mj_blog_post', 'mj_article', 'mj_full_width'),
+                'post_status' => 'publish'
+              )
+            ) ),
+          ) )
+        )
+      ) );
+    }
+
+    /* end article type fields */
+
+    /* begin author type fields */
+
+    public function position() {
+      return new Fieldmanager_TextField( array(
+        'name' => 'position',
+      ) );
+    }
+
+    public function image() {
+      return new Fieldmanager_Media( array( 
+        'name' => 'image'
+      ) );
+    }
+
+    public function long_bio() {
+      return new Fieldmanager_TextArea( array(
+        'name' => 'long_bio'
+      ) );
+    }
+
+    public function short_bio() {
+      return new Fieldmanager_TextArea( array(
+        'name' => 'short_bio'
+      ) );
+    }
+
+    public function twitter() {
+      return new Fieldmanager_TextField( array(
+        'name' => 'twitter',
+      ) );
+    }
+
+
+    /* end author type fields */
+
+    /* begin user type fields */
+    public function author() {
+      return new Fieldmanager_Autocomplete( array(
+        'name'      => 'author',
+        'datasource' => new Fieldmanager_Datasource_Post( array(
+          'query_args' => array( 
+            'post_type' => 'mj_author',
+            'post_status' => 'publish'
+          )
+        ) ),
+      ) );
+    }
+    /* end user type fields */
+  }
+
+}
