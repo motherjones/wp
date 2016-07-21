16e618d06b4400eeb7dc07e1907f686d7e7f3b46
ben <benbreedlove@gmail.com>
2016-07-21 16:42:42 -0400

    is it the multitypes?

--- wp-content/mu-plugins/mj-custom.php
+++ wp-content/mu-plugins/mj-custom.php
@@ -131,7 +131,8 @@ function related() {
         'add_more_label' => 'Add another article',
 				'datasource' => new Fieldmanager_Datasource_Post( array(
           'query_args' => array( 
-            'post_type' => array('mj_blog_post', 'mj_article', 'mj_full_width'),
+          //  'post_type' => array('mj_blog_post', 'mj_article', 'mj_full_width'),
+            'post_type' => 'mj_blog_post',
             'post_status' => 'publish'
           )
 				) ),
