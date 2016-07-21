8244a6bb3b8cf4f3ab64331a089c0b75ab1507fb
ben <benbreedlove@gmail.com>
2016-07-21 14:11:46 -0400

    ugh is it the query args?

--- wp-content/mu-plugins/mj-custom.php
+++ wp-content/mu-plugins/mj-custom.php
@@ -71,9 +71,9 @@ $byline = new Fieldmanager_Group( array(
   'name' => 'Byline',
   'children' => array(
     'authors' => new Fieldmanager_Autocomplete( "Authors", array(
-      'datasource' => new Fieldmanager_Datasource_Post( array(
+      'datasource' => new Fieldmanager_Datasource_Post(/* array(
         'query_args' => array( 'post_type' => 'mj_author' )
-      ) ),
+      ) */),
     ) ),
     'override' => new Fieldmanager_TextField( 'Byline Override' )
   )
