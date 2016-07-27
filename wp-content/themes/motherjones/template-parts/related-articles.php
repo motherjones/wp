af641f7a2f22df0d7c1a197779d8ffaf85779a51
ben <benbreedlove@gmail.com>
2016-07-27 19:25:31 -0400

    check out realteds

--- wp-content/themes/motherjones/template-parts/related-articles.php
+++ wp-content/themes/motherjones/template-parts/related-articles.php
@@ -3,11 +3,14 @@
     <ul class="related-articles-list">
       <?php 
         $related = get_post_field( 'related_articles', get_the_ID() ); 
+        print_r($related);
         $related_query = new WP_Query(array(
           'post__in' => $related['relateds']
         ) );
+        print_r($related_query);
         while ( $related_query->have_posts() ) : $related_query->the_post(); 
       ?>
+<h1>lookin for an article</h1>
 
         <?php get_template_part( 'standard-article-li', 'single' ); ?>
 
