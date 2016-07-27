fbf88940bc1b78b8f02130302e377cf6ba9b0dc6
ben <benbreedlove@gmail.com>
2016-07-27 19:46:52 -0400

    i dont get why getting this template part doesnt work

--- wp-content/themes/motherjones/template-parts/related-articles.php
+++ wp-content/themes/motherjones/template-parts/related-articles.php
@@ -3,9 +3,6 @@
     <ul class="related-articles-list">
       <?php 
         $related = get_post_field( 'related_articles', get_the_ID() ); 
-        print_r($related);
-        print_r($related['relateds']);
-        print_r(Array(60, 100));
         $related_query = new WP_Query(array(
           'post__in' => $related['relateds'],
           'post_type' => Array('mj_blog_post', 'mj_article', 'mj_full_width')
@@ -14,7 +11,8 @@
       ?>
 <h1>lookin for an article</h1>
 
-        <?php get_template_part( 'standard-article-li'); ?>
+        <?php get_template_part( 'standard-article-li' ); ?>
+        WHY DON"T YOU PRING THE PART UGH!
 
       <?php endwhile; ?>
     </ul>
