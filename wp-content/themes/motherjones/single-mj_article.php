2ed776efb3e7421eb4614d7d2d5202e275559310
ben <benbreedlove@gmail.com>
2016-07-27 18:32:14 -0400

    ugh show me the fields again

--- wp-content/themes/motherjones/single-mj_article.php
+++ wp-content/themes/motherjones/single-mj_article.php
@@ -11,6 +11,7 @@ get_header(); ?>
 <div id="primary" class="content-area">
 	<main id="main" class="site-main" role="main">
 <?php while ( have_posts() ) : the_post(); ?>
+<?php print_r( get_post_custom( get_the_ID() ) ); ?>
       <article id="post-<?php get_the_ID(); ?>" class="article">
         <?php print get_post_field( 'css', get_the_ID() ); ?>
         <header class="entry-header">
