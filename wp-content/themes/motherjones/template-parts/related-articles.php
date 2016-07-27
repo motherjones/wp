8d7334d23559670def030e662a67679c5501dfcc
ben <benbreedlove@gmail.com>
2016-07-27 19:35:25 -0400

    try this on for size

--- wp-content/themes/motherjones/template-parts/related-articles.php
+++ wp-content/themes/motherjones/template-parts/related-articles.php
@@ -4,11 +4,11 @@
       <?php 
         $related = get_post_field( 'related_articles', get_the_ID() ); 
         print_r($related);
+        print_r($related['relateds']);
+        print_r(Array(60, 100));
         $related_query = new WP_Query(array(
-          'post_type' => Array('mj_blogpost', 'mj_article', 'mj_fullwidth'),
-          //'post__in' => $related['relateds'] 
+          'post__in' => $related['relateds'] 
         ) );
-        print_r($related_query);
         while ( $related_query->have_posts() ) : $related_query->the_post(); 
       ?>
 <h1>lookin for an article</h1>
