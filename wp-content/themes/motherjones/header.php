1f854ab6ac8685004a2f7c82560e48072f12af79
ben <benbreedlove@gmail.com>
2016-07-28 15:54:05 -0400

    uhm crap now to think about responsiveness

--- wp-content/themes/motherjones/header.php
+++ wp-content/themes/motherjones/header.php
@@ -25,7 +25,6 @@
 <?php dynamic_sidebar( 'page-top' ); ?>
 <div id="page" class="site">
 	<div class="site-inner">
-		<a class="skip-link screen-reader-text" href="#content"><?php _e( 'Skip to content', 'twentysixteen' ); ?></a>
 
 		<header id="masthead" class="site-header" role="banner">
 			<?php get_template_part( 'template-parts/static-navbar'); ?>
