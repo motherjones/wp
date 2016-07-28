bba28cb6d1c272f94bf82fb0171747ca37d2c8af
ben <benbreedlove@gmail.com>
2016-07-28 16:09:04 -0400

    add js for floatnav, footer stuff

--- wp-content/themes/motherjones/footer.php
+++ wp-content/themes/motherjones/footer.php
@@ -14,23 +14,70 @@
 
 		<footer id="colophon" class="site-footer" role="contentinfo">
 
-			<div class="site-info">
-				<?php
-					/**
-					 * Fires before the twentysixteen footer text for footer customization.
-					 *
-					 * @since Twenty Sixteen 1.0
-					 */
-					do_action( 'twentysixteen_credits' );
-				?>
-				<span class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></span>
-				<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentysixteen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentysixteen' ), 'WordPress' ); ?></a>
-			</div><!-- .site-info -->
+      <a href="/" id="footer-logo">
+          <img src="/wp-content/themes/motherjones/img/MJ_comp_grey.png" alt="Mother Jones" />
+      </a>
+      <ul id="footer-social">
+          <li class="circled-icon toolbar-btn fblike">
+              <a href="https://www.facebook.com/motherjones">
+                  <i class="fa fa-facebook fw"></i>
+              </a>
+          </li>
+          <li class="circled-icon toolbar-btn tweet">
+              <a href="https://twitter.com/motherjones">
+                  <i class="fa fa-twitter fw"></i>
+              </a>
+          </li>
+          <li class="circled-icon toolbar-btn newsletter">
+              <a href="/about/interact-engage/free-email-newsletter" class="hover-newsletter">
+                <i class="fa fa-envelope-square fw"></i>
+              </a>
+          </li>
+      </ul>
+      <ul id="footer-list">
+          <li>
+              <a href="/about">
+                  About Us
+              </a>
+          </li>
+          <li>
+              <a href="/about/support">
+                  Donate
+              </a>
+|                    <a href="https://secure.motherjones.com/fnx/?action=SUBSCRIPTION&pub_code=MJM&term_pub=MJM&list_source=SEGYN4&base_country=US">
+                  Subscribe
+              </a>
+          </li>
+          <li>
+               <a href="/about/subscriptions/customer-service">
+                    Customer Service
+              </a>
+          </li>
+          <li>
+              <a href="/about/advertising/contact-form">
+                  Advertise
+              </a>
+          </li>
+      </ul>
+      <div id="copyright">
+          <p>
+              Copyright &copy;2016 Mother Jones and the Foundation for National Progress. All Rights Reserved.
+         </p>
+          <p>
+              <a href="/about/terms">Terms of Service</a>
+              <a href="/about/privacy-policy">Privacy Policy</a>
+              <a href="/about/contact">Contact Us</a>
+          </p>
+      </div>
+
 		</footer><!-- .site-footer -->
 	</div><!-- .site-inner -->
 </div><!-- .site -->
 
 <?php wp_footer(); ?>
+
+<script type="text/javascript" src="/wp-content/themes/motherjones/js/nav.js"></script>
+
 <?php dynamic_sidebar( 'page-end' ); ?>
 </body>
 </html>
