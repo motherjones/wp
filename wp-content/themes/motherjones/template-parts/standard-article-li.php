69a37ba6222544a77023202da6b5eaaecb365c79
ben <benbreedlove@gmail.com>
2016-07-29 14:34:45 -0400

    remove logging remove underlines try this out

--- wp-content/themes/motherjones/template-parts/standard-article-li.php
+++ wp-content/themes/motherjones/template-parts/standard-article-li.php
@@ -1,17 +1,20 @@
 <li class="article-item">
-  <a href="<?php print esc_url( get_permalink() ); ?>">
-<?php print_r(get_post_field( 'master_image', get_the_ID() )); ?>
-    <?php print wp_get_attachment_image( 
-      get_post_field( 'master_image', get_the_ID() )['master_image'],
-      '208'
-    ); ?>
-  </a>
-  <h3 class="hed">
+  <div class="article-image">
     <a href="<?php print esc_url( get_permalink() ); ?>">
-      <?php the_title(); ?>
+      <?php print wp_get_attachment_image( 
+        get_post_field( 'master_image', get_the_ID() )['master_image'],
+        '208'
+      ); ?>
     </a>
-  </h3>
-  <p class="byline">
-    <?php print mj_byline( get_the_ID() ); ?>
-  </p>
+  </div>
+  <div class="article-data">
+    <h3 class="hed">
+      <a href="<?php print esc_url( get_permalink() ); ?>">
+        <?php the_title(); ?>
+      </a>
+    </h3>
+    <p class="byline">
+      <?php print mj_byline( get_the_ID() ); ?>
+    </p>
+  </div>
 </li>
