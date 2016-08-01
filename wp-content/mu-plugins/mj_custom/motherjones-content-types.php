517249bce279822f79142f2f60af2bb338d04c6a
ben <benbreedlove@gmail.com>
2016-08-01 16:52:17 -0400

    uhhh not getting here maybe?

--- wp-content/mu-plugins/mj_custom/motherjones-content-types.php
+++ wp-content/mu-plugins/mj_custom/motherjones-content-types.php
@@ -236,7 +236,9 @@ if ( !class_exists( 'MJ_Custom_Types' ) ) {
 
 
     public function set_index_query( $query ) {
+      print_r('<h1>IS HERE</h1>');
       if(is_category() || is_tag() || is_tax()) {
+        print_r('<h1>IS PAST CONDITIONAL</h1>');
         print_r($sectionable_types);
         $post_type = get_query_var('post_type');
         if(!$post_type) { 
