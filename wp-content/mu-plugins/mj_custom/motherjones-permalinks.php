8d48fedd5fd5eed22e0861e5388c8e4646486905
ben <benbreedlove@gmail.com>
2016-08-02 16:21:08 -0400

    blog type is not cat type thanks

--- wp-content/mu-plugins/mj_custom/motherjones-permalinks.php
+++ wp-content/mu-plugins/mj_custom/motherjones-permalinks.php
@@ -25,8 +25,14 @@ if ( !class_exists( 'MJ_Permalinks' ) ) {
         // this is the actual manipulation; do whatever you need here
         if ($dummy_query->query['category_name'] && $dummy_query->query['name']) {
           $request['post_type'] = array('mj_article', 'mj_fullwidth', 'mj_blogpost');
+          if (get_terms( array(
+              'name' => $dummy_query->query['category_name'],
+              'taxonomy' => 'mj_blog_type'
+          ) ) ) {
+            unset($request['category_name']);
+          }
         }
-        print_r($dummy_query);
+        print_r($request);
 
         return $request;
     }
