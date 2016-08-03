603b579800f2f877af2b36fb2e8482649ac80600
ben <benbreedlove@gmail.com>
2016-08-03 17:50:49 -0400

    more work on fullwidths

--- wp-content/themes/motherjones/template-parts/homepage-fullwidth.php
+++ wp-content/themes/motherjones/template-parts/homepage-fullwidth.php
@@ -13,13 +13,13 @@
   <?php print get_post_field( 'master_image', get_the_ID() )['master_image_byline']; ?>
 </p>
 <div class="article-data">
-  <h3>
+  <h1 class="hed">
     <a href="<?php print esc_url( get_permalink() ); ?>">
         <?php print get_post_field( 'alt', get_the_ID() )['alt_title']
                   ? get_post_field( 'alt', get_the_ID() )['alt_title']
                   : get_the_title(); ?>
     </a>
-  </h3>
+  </h1>
   <h4 class="dek">
     <a href="<?php print esc_url( get_permalink() ); ?>">
       <?php print get_post_field( 'alt', get_the_ID() )['alt_dek']
