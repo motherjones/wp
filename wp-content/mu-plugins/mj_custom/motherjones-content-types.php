3bf028a2f1de95e17cb116beb19216606425c7d5
ben <benbreedlove@gmail.com>
2016-08-04 13:53:01 -0400

    pretty links for fullwidth

--- wp-content/mu-plugins/mj_custom/motherjones-content-types.php
+++ wp-content/mu-plugins/mj_custom/motherjones-content-types.php
@@ -151,9 +151,9 @@ if ( !class_exists( 'MJ_Custom_Types' ) ) {
         )
       );
 			global $wp_rewrite;
-      $article_url_structure = '/%category%/%year%/%monthnum%/%mj_article%';
-      $wp_rewrite->add_rewrite_tag("%mj_article%", '([^/]+)', "mj_article=");
-      $wp_rewrite->add_permastruct('mj_article', $article_url_structure, false);
+      $article_url_structure = '/%category%/%year%/%monthnum%/%mj_full_width%';
+      $wp_rewrite->add_rewrite_tag("%mj_full_width%", '([^/]+)', "mj_full_width=");
+      $wp_rewrite->add_permastruct('mj_full_width', $article_url_structure, false);
     }
 
 
