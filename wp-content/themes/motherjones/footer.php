d3b6e6a1e1cdc8dd254bdaaf92a76fc00f225c2e
ben <benbreedlove@gmail.com>
2016-08-04 16:08:52 -0400

    ugh its a min file okay

--- wp-content/themes/motherjones/footer.php
+++ wp-content/themes/motherjones/footer.php
@@ -76,7 +76,7 @@
 <?php wp_footer(); ?>
 
 <script type="text/javascript" src="/wp-content/themes/motherjones/js/nav.js"></script>
-<script type="text/javascript" src="/wp-content/themes/motherjones/js/jquery-3.1.0.js"></script>
+<script type="text/javascript" src="/wp-content/themes/motherjones/js/jquery-3.1.0.min.js"></script>
 
 <?php dynamic_sidebar( 'page-end' ); ?>
 </body>
