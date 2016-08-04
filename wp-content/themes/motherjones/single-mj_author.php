179c019acd7d815635041392ac304ac8f88de84a
ben <benbreedlove@gmail.com>
2016-08-04 19:26:55 -0400

    pass at author pages

--- wp-content/themes/motherjones/single-mj_author.php
+++ wp-content/themes/motherjones/single-mj_author.php
@@ -10,17 +10,46 @@ get_header(); ?>
 
 <div id="content" class="site-content">
 <div id="primary" class="content-area">
-	<main id="main" class="site-main" role="main">
+	<main id="main" class="site-main author" role="main">
 		<?php
 		// Start the loop.
-		while ( have_posts() ) : the_post();
+		while ( have_posts() ) : $author = the_post();
+    ?>
 
-			// Include the single post content template.
-			get_template_part( 'template-parts/content', 'single' );
+      <div class="author-image">
+        <?php
+          print wp_get_attachment_image( $author['image'][0], 
+            array('80', '80')
+          );
+        ?>
+      </div>
+      <div class="author-data">
+        <p class="author-bio byline">
+          <?php print get_the_title( $author_id ); ?>
+          <span class="author-position">
+            <?php print $author['position']; ?>
+          </span>
+        </p>
+        <p class="author-bio-text">
+          <?php print $author['long_bio'][0] ?>
+        </p>
+      </div>
 
+    <?php
 			// End of the loop.
 		endwhile;
 		?>
+    <ul class="article-list">
+      <?php
+        $authors_articles = get_posts( array(
+          'posts_per_page' => 20,
+          'post_type' => array('mj_full_width', 'mj_article', 'mj_blog_post'),
+        ) );
+        while ( $authors_articles->have_posts() ) : $authors_articles->the_post();
+          get_template_part( 'template-parts/standard-article-li');
+        endwhile;
+      ?>
+    </ul>
 
 	</main><!-- .site-main -->
 
