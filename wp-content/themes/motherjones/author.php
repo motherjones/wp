2d0fdc14ec31567c384e8e72b04e2d099b4af88d
ben <benbreedlove@gmail.com>
2016-08-05 17:13:07 -0400

    put sidebar on author pages

--- wp-content/themes/motherjones/author.php
+++ wp-content/themes/motherjones/author.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * The template for displaying all single posts and attachments
+ * The template for displaying authors
  *
  * @package WordPress
  * @subpackage Mother Jones
@@ -11,46 +11,51 @@ get_header(); ?>
 <div id="content" class="site-content">
 <div id="primary" class="content-area">
 	<main id="main" class="site-main author" role="main">
-		<?php global $wp_query;
-      $author_name = $wp_query->query['author_name'];
-      $author_query = new WP_User_Query( array( 
-        'search' => $author_name,
-        'search_columns' => array('user_nicename'),
-      ) );
-      $author = $author_query->get_results()[0];
-     ?>
+    <div class="main-index"> 
+      <?php global $wp_query;
+        $author_name = $wp_query->query['author_name'];
+        $author_query = new WP_User_Query( array( 
+          'search' => $author_name,
+          'search_columns' => array('user_nicename'),
+        ) );
+        $author = $author_query->get_results()[0];
+       ?>
 
-    <div class="author-bio group">
-      <div class="author-image">
-        <?php
-          print wp_get_attachment_image( $author->image[0], 
-            array('100', '100')
-          );
-        ?>
-      </div>
-      <div class="author-data">
-        <p class="author-bio-byline byline">
-          <?php print $author->display_name; ?>
-          <span class="author-position">
-            <?php print get_user_meta($author->id, 'position', true); ?>
-          </span>
-        </p>
-        <div class="author-bio-text">
-          <?php print get_user_meta($author->id, 'long_bio', true); ?>
+      <div class="author-bio group">
+        <div class="author-image">
+          <?php
+            print wp_get_attachment_image( $author->image[0], 
+              array('100', '100')
+            );
+          ?>
+        </div>
+        <div class="author-data">
+          <p class="author-bio-byline byline">
+            <?php print $author->display_name; ?>
+            <span class="author-position">
+              <?php print get_user_meta($author->id, 'position', true); ?>
+            </span>
+          </p>
+          <div class="author-bio-text">
+            <?php print get_user_meta($author->id, 'long_bio', true); ?>
+          </div>
         </div>
       </div>
+
+      <ul class="article-list">
+        <?php while ( have_posts() ) : the_post();
+            get_template_part( 'template-parts/standard-article-li');
+          endwhile;
+        ?>
+      </ul>
     </div>
 
-    <ul class="article-list">
-      <?php while ( have_posts() ) : the_post();
-          get_template_part( 'template-parts/standard-article-li');
-        endwhile;
-      ?>
-    </ul>
+    <div id="sidebar-right">
+      <?php dynamic_sidebar( 'sidebar-section' ); ?>
+    </div>
 
 	</main><!-- .site-main -->
 
 </div><!-- .content-area -->
 
-<?php dynamic_sidebar( 'sidebar-article' ); ?>
 <?php get_footer(); ?>
