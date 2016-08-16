57b9c026c87b5a5c9256ef74ffc7c5d54cd8643a
ben <benbreedlove@gmail.com>
2016-08-16 10:54:37 -0400

    auto unserializes?

--- wp-content/themes/motherjones/template-parts/master-image-630.php
+++ wp-content/themes/motherjones/template-parts/master-image-630.php
@@ -1,16 +1,11 @@
 
-<?php print_r(get_post_meta( get_the_ID(), 'master_image' )); ?>
 <h2>MASTER GOES HERE?</h2>
-<?php print_r(get_post_meta( get_the_ID(), 'master_image' )['master_image']); ?>
-is suppressed below
-<?php print_r(get_post_meta( get_the_ID(), 'master_image' )['master_image_suppress']); ?>
 <?php if (get_post_meta( get_the_ID(), 'master_image' )[0]['master_image']
       && !get_post_meta( get_the_ID(), 'master_image' )[0]['master_image_suppress']
 ): ?>
   <div class="article-master-image group">
     <?php 
-print_r(get_post_meta(get_the_ID(), 'master_image' ));
-    $master = unserialize(get_post_meta(get_the_ID(), 'master_image' )[0]);
+    $master = get_post_meta(get_the_ID(), 'master_image' )[0];
 print_r($master);
     print wp_get_attachment_image( 
       $master['master_image'],
