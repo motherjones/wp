8636121b044475a7eaa921e76697a9fbeb96d2f4
ben <benbreedlove@gmail.com>
2016-08-16 10:50:01 -0400

    yep try this out

--- wp-content/themes/motherjones/template-parts/master-image-630.php
+++ wp-content/themes/motherjones/template-parts/master-image-630.php
@@ -4,12 +4,13 @@
 <?php print_r(get_post_meta( get_the_ID(), 'master_image' )['master_image']); ?>
 is suppressed below
 <?php print_r(get_post_meta( get_the_ID(), 'master_image' )['master_image_suppress']); ?>
-<?php if (get_post_meta( get_the_ID(), 'master_image' )['master_image']
-      && !get_post_meta( get_the_ID(), 'master_image' )['master_image_suppress']
+<?php if (get_post_meta( get_the_ID(), 'master_image' )[0]['master_image']
+      && !get_post_meta( get_the_ID(), 'master_image' )[0]['master_image_suppress']
 ): ?>
   <div class="article-master-image group">
     <?php 
     $master = unserialize(get_post_meta(get_the_ID(), 'master_image' ));
+print_r($master);
     print wp_get_attachment_image( 
       $master['master_image'],
       'article_top'
