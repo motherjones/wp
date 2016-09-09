7c53f2c979014df3fab698eb5f216c85537e2e7e
ben <benbreedlove@gmail.com>
2016-09-09 13:19:48 -0400

    full widths sometimes dont have title iamges

--- wp-content/themes/motherjones/single-mj_full_width.php
+++ wp-content/themes/motherjones/single-mj_full_width.php
@@ -9,28 +9,45 @@
 get_header(); ?>
 
 <header id="full-width-header" class="group">
-  <div id="full-width-header-image">
-    <?php print wp_get_attachment_image( 
-      get_post_meta( get_the_ID(), 'full_width_title_image' )[0]['title_image'],
-      'full_width_giant'
-    ); ?>
-  </div>
-  <div id="full-width-header-data">
-    <?php the_title( '<h1 class="article hed">', '</h1>' ); ?>
-    <?php if ( get_post_meta( get_the_ID(), 'dek' ) ): ?>
-      <h3 class="dek">
-        <?php print get_post_meta( get_the_ID(), 'dek' )[0]; ?>
-      </h3>
-    <?php endif; ?>
-    <p class="byline-dateline">
-      <span class="byline">
-        <?php print mj_byline( get_the_ID() ); ?>
-      </span>
-      <span class="dateline">
-        <?php print mj_dateline( get_the_ID() ); ?>
-      </span>
-    </p>
-  </div>
+  <?php if ( get_post_meta( get_the_ID(), 'full_width_title_image' )[0]['title_image'] ): ?>
+    <div id="full-width-header-image">
+      <?php print wp_get_attachment_image( 
+        get_post_meta( get_the_ID(), 'full_width_title_image' )[0]['title_image'],
+        'full_width_giant'
+      ); ?>
+    </div>
+    <div id="full-width-header-data">
+      <?php the_title( '<h1 class="article hed">', '</h1>' ); ?>
+      <?php if ( get_post_meta( get_the_ID(), 'dek' ) ): ?>
+        <h3 class="dek">
+          <?php print get_post_meta( get_the_ID(), 'dek' )[0]; ?>
+        </h3>
+      <?php endif; ?>
+      <p class="byline-dateline">
+        <span class="byline">
+          <?php print mj_byline( get_the_ID() ); ?>
+        </span>
+        <span class="dateline">
+          <?php print mj_dateline( get_the_ID() ); ?>
+        </span>
+      </p>
+    </div>
+  <?php else: ?>
+      <?php the_title( '<h1 class="article hed">', '</h1>' ); ?>
+      <?php if ( get_post_meta( get_the_ID(), 'dek' ) ): ?>
+        <h3 class="dek">
+          <?php print get_post_meta( get_the_ID(), 'dek' )[0]; ?>
+        </h3>
+      <?php endif; ?>
+      <p class="byline-dateline">
+        <span class="byline">
+          <?php print mj_byline( get_the_ID() ); ?>
+        </span>
+        <span class="dateline">
+          <?php print mj_dateline( get_the_ID() ); ?>
+        </span>
+      </p>
+  <?php endif; ?>
 </header>
 <p class="homepage-art-byline">
   <?php print get_post_meta( get_the_ID(), 'full_width_title_image'  )[0]['title_image_byline']; ?>
