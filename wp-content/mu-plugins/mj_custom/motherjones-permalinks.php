d16ab86957e74b052be27c13318dddd894f95de5
ben <benbreedlove@gmail.com>
2016-10-04 15:49:56 -0400

    remove logging now

--- wp-content/mu-plugins/mj_custom/motherjones-permalinks.php
+++ wp-content/mu-plugins/mj_custom/motherjones-permalinks.php
@@ -22,7 +22,6 @@ if ( !class_exists( 'MJ_Permalinks' ) ) {
         $dummy_query = new WP_Query();  // the query isn't run if we don't pass any query vars
         $dummy_query->parse_query( $request );
 
-          print_r($request);
         // this is the actual manipulation; do whatever you need here
         if ($dummy_query->query['category_name'] && $dummy_query->query['name']) {
           $request['post_type'] = array('mj_article', 'mj_full_width');
@@ -37,7 +36,6 @@ if ( !class_exists( 'MJ_Permalinks' ) ) {
               'terms' => $request['category_name'],
             ) );
             unset($request['category_name']);
-            print_r($request);
           }
         } elseif ( preg_match('/^author\//', $dummy_query->query['category_name']) ) { //is author
           $request['post_type'] = array('mj_article', 'mj_full_width', 'mj_blog_post');
@@ -54,7 +52,6 @@ if ( !class_exists( 'MJ_Permalinks' ) ) {
             'field' => 'slug',
             'terms' => $request['author_name'],
           ) );
-          print_r($request);
           unset($request['category_name']);
           unset($request['year']);
         }  elseif ( //is topic
@@ -73,7 +70,6 @@ if ( !class_exists( 'MJ_Permalinks' ) ) {
               'terms' => $request['category_name'],
             ) );
             unset($request['category_name']);
-          print_r($request);
         }  elseif ( //is media type
           !get_terms( array(
             'taxonomy' => 'category', 
@@ -90,7 +86,6 @@ if ( !class_exists( 'MJ_Permalinks' ) ) {
               'terms' => $request['category_name'],
             ) );
             unset($request['category_name']);
-          print_r($request);
         }  elseif ( //is blog posts
           !get_terms( array(
             'taxonomy' => 'category', 
@@ -107,7 +102,6 @@ if ( !class_exists( 'MJ_Permalinks' ) ) {
               'terms' => $request['category_name'],
             ) );
             unset($request['category_name']);
-          print_r($request);
         }
         return $request;
     }
