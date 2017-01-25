3c37481eb805166b33b5ea5cda2020cc83e3a281
ben <benbreedlove@gmail.com>
2017-01-25 14:24:59 -0500

    add a way to resize video embeds

--- wp-content/themes/motherjones/functions.php
+++ wp-content/themes/motherjones/functions.php
@@ -373,6 +373,8 @@ function twentysixteen_scripts() {
 
 	wp_enqueue_script( 'twentysixteen-script', get_template_directory_uri() . '/js/functions.js', array( 'jquery' ), '20150825', true );
 
+	wp_enqueue_script( 'twentysixteen-script', get_template_directory_uri() . '/js/video-embed.js', array( 'jquery' ), '20170125', true );
+
 	wp_localize_script( 'twentysixteen-script', 'screenReaderText', array(
 		'expand'   => __( 'expand child menu', 'twentysixteen' ),
 		'collapse' => __( 'collapse child menu', 'twentysixteen' ),
---
+++ wp-content/themes/motherjones/js/video-embed.js
@@ -0,0 +1,33 @@
+var mj_fix_embed = function(embed) {
+    if (embed.attr('width').match('%')) {
+      embed.attr('height', 'auto');
+      return;
+    } else if (embed.attr('height').match('%')) {
+      return;
+    }
+    var div = jQuery('<div class="mobile_video_embed_container"></div>');
+    var padding_bottom = parseInt(embed.attr('height')) 
+        / parseInt(embed.attr('width')) 
+        * 100;
+    embed.attr('style', '');
+    div.css('padding-bottom', padding_bottom + '%')
+    embed.before(div);
+    div.append(embed);
+}
+
+jQuery(document).ready(function() {
+    jQuery('iframe, object, embed').each(function() {
+        var $this = jQuery(this);
+        var url = $this.attr('src') || $this.html();
+        if (
+               url.match(/youtu/)
+            || url.match(/appspot/)
+            || url.match(/vimeo/)
+            || url.match(/msnbc/)
+            || url.match(/livestream/)
+            || url.match(/facebook.com\/plugins\/video.php/)
+        ) {
+            mj_fix_embed($this);
+        }
+    });
+});
