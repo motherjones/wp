994db1903bb3813679e78f071ba91db4a98465b8
ben <benbreedlove@gmail.com>
2017-02-01 19:18:01 -0500

    pring the request

--- wp-content/themes/motherjones/header.php
+++ wp-content/themes/motherjones/header.php
@@ -50,6 +50,9 @@
 </head>
 
 <body <?php body_class(); ?>>
+<?php
+				print_r( $request );
+?>
 <?php get_template_part( 'template-parts/floating-navbar'); ?>
 <?php dynamic_sidebar( 'page-top' ); ?>
 <div id="page" class="site">
