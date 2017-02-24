343a989fe1e5f2bd571b938cd66513e6dd936cc2
bbreedlove <bbreedlove@motherjones.com>
2017-02-24 15:51:29 +0000

    fix deploy script, add image resizing

--- wp_stage_migration.sh
+++ wp_stage_migration.sh
@@ -1,7 +1,10 @@
 #!/bin/bash          
-DD=$(date +%d)
-MM=$(date +%m)
-YYYY=$(date +%Y)
+DD=$(date +%d -d "1 day ago")
+MM=$(date +%m -d "1 day ago")
+YYYY=$(date +%Y -d "1 day ago")
+#DD=$(date +%d)  #this is for the same day, uncomment me if we're pushin for real
+#MM=$(date +%m)
+#YYYY=$(date +%Y 
 
 FILES_DIR='/mojo/www/bd6_files/' #point this at the most recent drupal files dir you got
 ENV='dev'
@@ -15,61 +18,96 @@ echo "Whoops, need password"
 exit
 fi
 
+echo "\n"
+
 read -sp "Pantheon Password: " PANT_PASS
 if [[ -z "$PANT_PASS" ]]; then
 echo "Whoops, need password"
 exit
 fi
 
+echo "\n"
+
 read -sp "Pantheon Database Password: " PANT_SQL_PASS
 if [[ -z "$PANT_SQL_PASS" ]]; then
 echo "Whoops, need password"
 exit
 fi
+echo "\n"
 
-#scp -r  motherjones_d6.prod@web-197.prod.hosting.acquia.com:/mnt/files/motherjones_d6/backups/prod-motherjones_d6-motherjones_d6-$YYYY-$MM-$DD.sql.gz . 
+scp -r  motherjones_d6.prod@web-197.prod.hosting.acquia.com:/mnt/files/motherjones_d6/backups/prod-motherjones_d6-motherjones_d6-$YYYY-$MM-$DD.sql.gz . 
 
 echo "Prod db backup downloaded"
+echo "\n"
 
-#gunzip prod-motherjones_d6-motherjones_d6-$YYYY-$MM-$DD.sql.gz
+
+gunzip prod-motherjones_d6-motherjones_d6-$YYYY-$MM-$DD.sql.gz
 
 echo "Prod db unzipped"
+echo "\n"
 
-#mysql -u root -p"$SQL_PASS" mjd6 < prod-motherjones_d6-motherjones_d6-$YYYY-$MM-$DD.sql
+
+mysql -u root -p"$SQL_PASS" mjd6 < prod-motherjones_d6-motherjones_d6-$YYYY-$MM-$DD.sql
 
 echo "prod db pushed into database"
+echo "\n"
 
-#php migrate_database.php "$SQL_PASS"
+
+php migrate_database.php "$SQL_PASS"
 
 echo "prod db moved to wp db"
+echo "\n"
 
-#php migrate_blocks.php "$SQL_PASS"
+
+php migrate_blocks.php "$SQL_PASS"
 
 echo "blocks added to wp db"
+echo "\n"
 
-#mysqldump -u root -p"$SQL_PASS" pantheon_wp > migrated-wp-db-$YYYY-$MM-$DD.sql
+
+mysqldump -u root -p"$SQL_PASS" pantheon_wp > migrated-wp-db-$YYYY-$MM-$DD.sql
 
 echo "wp db dumped to file at migrated-wp-db-$YYYY-$MM-$DD.sql"
+echo "\n"
 
-#mysql -u pantheon -p"$PANT_SQL_PASS" -h dbserver.dev.$SITE.drush.in -P "$PANT_SQL_PORT" pantheon < migrated-wp-db-$YYYY-$MM-$DD.sql
+
+mysql -u pantheon -p"$PANT_SQL_PASS" -h dbserver.dev.$SITE.drush.in -P "$PANT_SQL_PORT" pantheon < migrated-wp-db-$YYYY-$MM-$DD.sql
 
 echo "wp db uploaded to pantheon"
+echo "\n"
 
-#rsync -rtvzl --chmod=ug+rwX -e "ssh -i /home/bbreedlove/.ssh/id_rsa" --exclude=/boost* --exclude=/cache-tmpfs* --exclude=/js* --exclude=/css* motherjones_d6.prod@web-197.prod.hosting.acquia.com:/mnt/files/motherjones_d6/files/ $FILES_DIR
+
+rsync -rtvzl --chmod=ug+rwX -e "ssh -i /home/bbreedlove/.ssh/id_rsa" --exclude=/boost* --exclude=/cache-tmpfs* --exclude=/js* --exclude=/css* motherjones_d6.prod@web-197.prod.hosting.acquia.com:/mnt/files/motherjones_d6/files/ $FILES_DIR
 
 echo "pulled prod files to pastina"
+echo "\n"
 
 
-while [ 1 ]
+
+PSYNCED=false
+until [ "$PSYNCED" = true ] ;
 do
-  sshpass -p "$PANT_PASS" rsync --exclude=/imagefield_thumbs* --exclude=/resized* --exclude=/imagecache* --exclude=/cache-tmpfs* --exclude=/js* --exclude=/css* --partial -rlvz --size-only --ipv4 --progress -e 'ssh -o StrictHostKeyChecking=no -p 2222' $FILES_DIR --temp-dir=../tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/ 
+  sshpass -p "$PANT_PASS" rsync --exclude=/imagefield_thumbs* --exclude=/resized* --exclude=/cloudfront_queue* --exclude=/imagecache* --exclude=/cache-tmpfs* --exclude=/js* --exclude=/css* --partial -rlvz --no-links --size-only --ipv4 --progress -e 'ssh -o StrictHostKeyChecking=no -p 2222' $FILES_DIR --temp-dir=../tmp/ $ENV.$SITE@appserver.$ENV.$SITE.drush.in:files/ 
 if [ "$?" = "0" ] ; then
 echo "rsync completed normally"
-exit
+PSYNCED=true
 else
 echo "Rsync failure. Backing off and retrying..."
 sleep 180
 fi
   
 done
+echo "\n"
+
+
+
+expect <<- DONE
+spawn /home/bbreedlove/vendor/bin/terminus remote:wp $SITE.$ENV media regenerate
+
+expect  "*?assword:*" { send "$PANT_PASS\r"  }
+
+expect "y/n*" { send "y\r" } 
+
+expect eof
+DONE
 
