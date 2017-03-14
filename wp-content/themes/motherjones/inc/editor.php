22bf7122b56dd4f211ceaafdd7d1bf54262eaeae
aschweigert <aschweigert@gmail.com>
2017-03-14 14:36:04 -0400

    title -> headline, re-add paragraph to formats dropdown

--- wp-content/themes/motherjones/inc/editor.php
+++ wp-content/themes/motherjones/inc/editor.php
@@ -7,65 +7,68 @@
  * @since Mother Jones 1.0
  */
 
- /**
-  * set wysiwyg config
-  */
-if ( ! function_exists( 'mj_wysiwyg_config' ) ) {
-   function mj_wysiwyg_config($config) {
- 		$config['remove_linebreaks'] = false;
- 		$config['gecko_spellcheck'] = true;
- 		$config['keep_styles'] = true;
- 		$config['accessibility_focus'] = true;
- 		$config['tabfocus_elements'] = 'major-publishing-actions';
- 		$config['media_strict'] = false;
- 		$config['paste_remove_styles'] = true;
- 		$config['paste_remove_spans'] = true;
- 		$config['paste_strip_class_attributes'] = 'none';
- 		$config['paste_text_use_dialog'] = true;
- 		$config['wpeditimage_disable_captions'] = true;
- 		$config['wpautop'] = true;
- 		$config['apply_source_formatting'] = false;
+/**
+ * Set wysiwyg config.
+ *
+ * @param array $config the original tinymce config.
+ */
+function mj_wysiwyg_config( $config ) {
+	$config['remove_linebreaks'] = false;
+	$config['gecko_spellcheck'] = true;
+	$config['keep_styles'] = true;
+	$config['accessibility_focus'] = true;
+	$config['tabfocus_elements'] = 'major-publishing-actions';
+	$config['media_strict'] = false;
+	$config['paste_remove_styles'] = true;
+	$config['paste_remove_spans'] = true;
+	$config['paste_strip_class_attributes'] = 'none';
+	$config['paste_text_use_dialog'] = true;
+	$config['wpeditimage_disable_captions'] = true;
+	$config['wpautop'] = true;
+	$config['apply_source_formatting'] = false;
 
- 		$style_formats = array(
- 			array(
- 				'title' => 'Subheader',
- 				'block' => 'h3',
- 				'classes' => 'subheader',
- 				'wrapper' => false,
- 			),
- 			array(
- 				'title' => 'Section Lead',
- 				'inline' => 'span',
- 				'classes' => 'section-lead',
- 				'wrapper' => false,
- 			),
- 			array(
- 				'title' => 'Pullquote',
- 				'block' => 'blockquote',
- 				'classes' => 'pullquote-left',
- 				'wrapper' => false,
- 			),
- 		);
- 		// Insert the array, JSON ENCODED, into 'style_formats'
- 		$config['style_formats'] = json_encode( $style_formats );
-
- 		$config['toolbar1'] = 'bold,italic,strikethrough,bullist,numlist,blockquote,hr,styleselect,link,unlink,wp_fullscreen,wp_adv ';
- 		$config['toolbar2'] = 'spellchecker,underline,removeformat,charmap,outdent,indent,undo,redo,wp_help ';
-
- 		return $config;
-   }
+	$style_formats = array(
+		array(
+			'title' => 'Paragraph',
+			'block' => 'p',
+			'wrapper' => false,
+		),
+		array(
+			'title' => 'Subheader',
+			'block' => 'h3',
+			'classes' => 'subheader',
+			'wrapper' => false,
+		),
+		array(
+			'title' => 'Section Lead',
+			'inline' => 'span',
+			'classes' => 'section-lead',
+			'wrapper' => false,
+		),
+		array(
+			'title' => 'Pullquote',
+			'block' => 'blockquote',
+			'classes' => 'pullquote-left',
+			'wrapper' => false,
+		),
+	);
+	// Insert the array, JSON ENCODED, into 'style_formats'.
+	$config['style_formats'] = wp_json_encode( $style_formats );
+	$config['toolbar1'] = 'bold,italic,strikethrough,bullist,numlist,blockquote,hr,styleselect,link,unlink,wp_fullscreen,wp_adv ';
+	$config['toolbar2'] = 'spellchecker,underline,removeformat,charmap,outdent,indent,undo,redo,wp_help ';
+	return $config;
 }
-//change default height of text editing area in wysiwyg
-add_action( 'admin_head', 'content_textarea_height' );
-function content_textarea_height() {
-	echo'
-        <style type="text/css">
-					.post-type-mj_blog_post #fm-body-0_ifr {height: 280px !important;}
-					.post-type-mj_blog_post #fm-body-0 {height: 280px !important;}
-					.post-type-mj_article #fm-body-0_ifr {height: 560px !important;}
-					.post-type-mj_article #fm-body-0 {height: 560px !important;}
-					.post-type-mj_full_width #fm-body-0_ifr {height: 840px !important;}
-					.post-type-mj_full_width #fm-body-0 {height: 840px !important;}
-        </style>
-	';
+
+/**
+ * Change the "enter title here" prompt for posts
+ *
+ * @param string $title the original placeholder text.
+ */
+function mj_change_title_text( $title ) {
+	$screen = get_current_screen();
+	if ( 'post' === $screen->base ) {
+		$title = 'Enter headline';
+	}
+	return $title;
 }
+add_filter( 'enter_title_here', 'mj_change_title_text' );
