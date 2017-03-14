9647eb3f48f79119e3f041432f2e627a0a8f1031
aschweigert <aschweigert@gmail.com>
2017-03-14 11:09:28 -0400

    coding standards

--- wp-content/themes/motherjones/archive.php
+++ wp-content/themes/motherjones/archive.php
@@ -20,62 +20,63 @@ get_header(); ?>
 
 <div id="content" class="site-content">
 	<div id="primary" class="content-area">
-    <main id="main" class="site-main group" role="main">
+		<main id="main" class="site-main group" role="main">
 
-	    <div class="main-index">
+			<div class="main-index">
 			<?php if ( have_posts() ) : ?>
 
 				<div class="page-header">
-	        <h1 class="page-title promo">
-	          <?php
-	            if ( is_tax() || is_category() ) {
-	              global $wp_query;
-	              $term = $wp_query->get_queried_object();
-	              print $term->name;
-	            } else {
-	              the_archive_title();
-	            }
-	          ?>
-	        </h1>
+					<h1 class="page-title promo">
+					<?php
+						if ( is_tax() || is_category() ) {
+							global $wp_query;
+							$term = $wp_query->get_queried_object();
+							print esc_html( $term->name );
+						} else {
+							the_archive_title();
+						}
+					?>
+					</h1>
 				</div><!-- .page-header -->
 
-	      <ul class="articles-list">
-	      <?php
-		      $posts_shown = 0;
+				<ul class="articles-list">
+				<?php
+					$posts_shown = 0;
 
 					// Start the Loop.
 					while ( $wp_query->have_posts() ) : $wp_query->the_post();
-		        if ( $posts_shown === 1 ) {
-		          get_template_part( 'template-parts/top-index-article' );
-		        } else {
-		          get_template_part( 'template-parts/standard-article-li' );
-		        }
-		        $posts_shown++;
 
-		        if ( $posts_shown === 5 ) : ?>
-		          <script>
-		            ad_code({
-		                yieldmo: true,
-		               docwrite: true,
-		                desktop: false,
-		              placement: 'ym_869408394552483686',
-		            });
-		          </script>
-		        <?php endif;
+					if ( 1 === $posts_shown ) {
+						get_template_part( 'template-parts/top-index-article' );
+					} else {
+						get_template_part( 'template-parts/standard-article-li' );
+					}
+					$posts_shown++;
+
+					if ( 5 === $posts_shown ) : ?>
+							<script>
+								ad_code({
+										yieldmo: true,
+									 docwrite: true,
+										desktop: false,
+									placement: 'ym_869408394552483686',
+								});
+							</script>
+					<?php endif;
 					// End the loop.
 					endwhile;
-		    ?>
-		    </ul>
-		    <div id="pager">
-		      <span class="pager_previous">
-		        <?php previous_posts_link( 'Previous' ); ?>
-		      </span>
-		      <span class="pager_next">
-		        <?php next_posts_link( 'Next' ); ?>
-		      </span>
-		    </div>
+				?>
+				</ul>
+				<div id="pager">
+					<span class="pager_previous">
+						<?php previous_posts_link( 'Previous' ); ?>
+					</span>
+					<span class="pager_next">
+						<?php next_posts_link( 'Next' ); ?>
+					</span>
+				</div>
 
-		    <?php
+				<?php
 				// If no content, include the "No posts found" template.
 				else :
 					get_template_part( 'template-parts/content', 'none' );
@@ -84,30 +85,30 @@ get_header(); ?>
 				?>
 			</div> <!-- .main-index -->
 
-	    <div id="sidebar-right">
-	      <script language="javascript">
-	          <!--
-	          if ( typeof MJ_HideRightColAds === 'undefined' ) {
-	            ad_code({
-	              desktop: true,
-	              placement: 'RightTopROS300x600',
-	              height: 529,
-	              doc_write: true,
-	            });
-	          }
-	          //-->
-	      </script>
-	      <?php dynamic_sidebar( 'sidebar-section' ); ?>
-	    </div>
+			<div id="sidebar-right">
+				<script language="javascript">
+						<!--
+						if ( typeof MJ_HideRightColAds === 'undefined' ) {
+							ad_code({
+								desktop: true,
+								placement: 'RightTopROS300x600',
+								height: 529,
+								doc_write: true,
+							});
+						}
+						//-->
+				</script>
+				<?php dynamic_sidebar( 'sidebar-section' ); ?>
+			</div>
 
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
-  <script>
-    ad_code({
-      yieldmo: true,
-     docwrite: true,
-      desktop: false,
-      placement: 'ym_869408549909503847',
-    });
-  </script>
+	<script>
+		ad_code({
+			yieldmo: true,
+		 	docwrite: true,
+			desktop: false,
+			placement: 'ym_869408549909503847',
+		});
+	</script>
 	<?php get_footer(); ?>
