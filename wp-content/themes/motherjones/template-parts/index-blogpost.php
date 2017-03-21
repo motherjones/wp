f6a11da81f9c2f055c1156186acfa31a85ae805d
aschweigert <aschweigert@gmail.com>
2017-03-20 20:59:13 -0400

    style blog social icons

--- wp-content/themes/motherjones/template-parts/index-blogpost.php
+++ wp-content/themes/motherjones/template-parts/index-blogpost.php
@@ -1,46 +1,48 @@
 <article class="blog">
-  <div class="entry-header blog">
-    <h1 class="blog hed">
-      <a href="<?php print esc_url( get_permalink() ); ?>">
-        <?php the_title(); ?>
-      </a>
-    </h1>
-    <?php if ( get_post_meta( get_the_ID(), 'mj_dek' )[0] ): ?>
-      <h3 class="dek">
-        <?php print get_post_meta( get_the_ID(), 'mj_dek' )[0]; ?>
-      </h3>
-    <?php endif ?>
-    <p class="byline-dateline">
-      <span class="byline">
-        <?php print mj_byline( get_the_ID() ); ?>
-      </span>
-      <span class="dateline">
-        <?php print mj_dateline( get_the_ID() ); ?>
-      </span>
-    </p>
-  </div><!-- .entry-header -->
-  <style>
-    <?php print get_post_meta( get_the_ID(), 'css' )[0]; ?>
-  </style>
+	<div class="entry-header blog">
+		<h1 class="blog hed">
+			<a href="<?php print esc_url( get_permalink() ); ?>">
+				<?php the_title(); ?>
+			</a>
+		</h1>
+		<?php
+			if ( ! empty( $meta['mj_dek'][0] ) ) {
+				printf(
+					'<h3 class="dek">%s</h3>',
+					esc_html( $meta['mj_dek'][0] )
+				);
+			}
+		?>
+		<p class="byline-dateline">
+			<span class="byline">
+				<?php print mj_byline( get_the_ID() ); ?>
+			</span>
+			<span class="dateline">
+				<?php print mj_dateline( get_the_ID() ); ?>
+			</span>
+		</p>
+	</div><!-- .entry-header -->
 
-  <?php get_template_part( 'template-parts/master-image-630' ); ?>
+	<?php
+		if ( isset( $meta['css'][0] ) ) {
+			printf(
+				'<style>%s</style>',
+				esc_html( $meta['css'][0] )
+			);
+		}
+		largo_hero();
+		the_content();
+	?>
 
-  <?php print get_post_meta( get_the_ID(), 'body' )[0]; ?>
-  <script>
-    <?php print get_post_meta( get_the_ID(), 'js' )[0]; ?>
-  </script>
-
-  <footer class="entry-footer">
-    <div class="social-container blog">
-      <ul class="social-tools blog">
-        <li class="twitter">
-          <?php print mj_flat_twitter_button( get_the_ID() ); ?>
-        </li>
-        <li class="facebook">
-          <?php print mj_flat_facebook_button( get_the_ID() ); ?>
-        </li>
-        <li>FIXME add disqus button</li>
-      </ul>
-    </div>
-  </footer>
+	<footer class="entry-footer">
+		<?php mj_share_tools( 'blog' );?>
+	</footer>
+	<?php
+		if ( ! empty( $meta['js'][0] ) ) {
+			printf(
+				'script>%s</script>',
+				$meta['js'][0]
+			);
+		}
+	?>
 </article>
