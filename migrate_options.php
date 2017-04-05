11e759041eea354666fee41c6059d759ff1b9ef5
ben <benbreedlove@gmail.com>
2017-04-05 15:12:08 -0400

    set analytics and ads for fb instant rss

--- migrate_options.php
+++ migrate_options.php
@@ -39,6 +39,20 @@ $wp->beginTransaction();
 $option_replace->execute(Array('posts_per_page', 20));
 $wp->commit();
 
+// set facebook instant ads and analytics
+$wp->beginTransaction();
+$option_replace->execute(Array(
+  'instant-articles-option-ads',
+  '{"ad_source":"iframe","fan_placement_id":"","iframe_url":"\/\/adserver.adtechus.com\/adiframe\/3.0\/5443.1\/4101495\/0\/170\/ADTECH;target=_blank","embed_code":"<script src=\"http:\/\/adserver.adtechus.com\/addyn\/3.0\/5443.1\/4101495\/0\/170\/ADTECH;loc=700;\"><\/script>","dimensions":"300x250"}'
+));
+$analytics_code = <<<'HTML'
+{"embed_code_enabled":"1","embed_code":"<script>\r\n  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\r\n  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\r\n  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\r\n  })(window,document,'script','https:\/\/www.google-analytics.com\/analytics.js','ga');\r\n  ga('create', 'UA-2458520-1', 'auto');\r\n  var dimensionValue = 'Facebook_Instant';\r\n  ga('set', 'dimension2', dimensionValue);\r\n  ga('set', 'campaignSource', 'Facebook');\r\n  ga('set', 'campaignMedium', 'Social Instant Article');\r\n  ga('send', 'pageview');\r\n  ga('send', 'event', 'Author', \"$byline\", window.location.href);\r\n\r\n    var _sf_async_config={};\r\n    \/** CONFIGURATION START **\/ \r\n    _sf_async_config.uid = 10683;\r\n    _sf_async_config.domain = 'motherjones.com';\r\n    _sf_async_config.sections = ''; \r\n    _sf_async_config.authors = ''; \r\n    \/** CONFIGURATION END **\/ \r\n    window._sf_endpt = (new Date()).getTime();\r\n var _comscore = _comscore || [];\r\n _comscore.push({ c1: \"2\", c2: \"8027488\", comscorekw: \"fbia\" });\r\n (function() {\r\n   var s = document.createElement(\"script\"), el = document.getElementsByTagName(\"script\")[0]; s.async = true;\r\n   s.src = (document.location.protocol == \"https:\" ? \"https:\/\/sb\" : \"http:\/\/b\") + \".scorecardresearch.com\/beacon.js\";\r\n   el.parentNode.insertBefore(s, el);\r\n })();\r\n\r\n<\/script>"}
+HTML;
+$option_replace->execute(Array( 
+  'instant-articles-option-analytics',
+	$analytics_code
+));
+$wp->commit();
 
 // set permalink structure
 $wp->beginTransaction();
