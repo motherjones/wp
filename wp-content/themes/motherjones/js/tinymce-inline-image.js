8fe0c6b79bda54b39057989a91a8fdfc74440a52
ben <benbreedlove@gmail.com>
2017-04-28 16:51:13 -0400

    add custom toolbar menu

--- wp-content/themes/motherjones/js/tinymce-inline-image.js
+++ wp-content/themes/motherjones/js/tinymce-inline-image.js
@@ -1,13 +1,56 @@
 /* global tinymce */
 tinymce.PluginManager.add('wpgallery', function( editor ) {
 
+	function image_align(name, node) {
+		console.log('ehre');
+		var wrap = editor.dom.getParent( node, 'figure.inline-image' );
+		if ( wrap ) {
+			node = wrap;
+		}
+		var class_attr = name;
+		if ( node.nodeName === 'FIGURE' ) {
+			class_attr += ' inline-image';
+		}
+			console.log(node);
+		editor.dom.setAttrib(node, 'class', class_attr);
+	}
+
+	function removeImage( node ) {
+		var wrap = editor.dom.getParent( node, 'figure.inline-image' );
+
+		if ( ! wrap && node.nodeName === 'IMG' ) {
+			wrap = editor.dom.getParent( node, 'a' );
+		}
+
+		if ( wrap ) {
+			if ( wrap.nextSibling ) {
+				editor.selection.select( wrap.nextSibling );
+			} else if ( wrap.previousSibling ) {
+				editor.selection.select( wrap.previousSibling );
+			} else {
+				editor.selection.select( wrap.parentNode );
+			}
+
+			editor.selection.collapse( true );
+			editor.dom.remove( wrap );
+		} else {
+			editor.dom.remove( node );
+		}
+
+		editor.nodeChanged();
+		editor.undoManager.add();
+	}
+
+	function isPlaceholder( node ) {
+		return !! ( editor.dom.getAttrib( node, 'data-mce-placeholder' ) || editor.dom.getAttrib( node, 'data-mce-object' ) );
+	}
+
 	function replaceInlineImageShortcodes( content ) {
 		return wp.shortcode.replace( 'inline_image', content, inline_image_shortcode );
 	}
 
 	function inline_image_shortcode( shortcode ) {
 		var content = wp.shortcode.replace('caption', shortcode.content, caption_shortcode);
-		console.log('image content is ', content);
 		var id = shortcode.attrs['named']['id'] ? 'id="' + shortcode.attrs['named']['id'] + '"' : '';
 		return '<figure ' + id + ' class="inline-image align' + 
 				shortcode.attrs['named']['align'] 
@@ -18,12 +61,10 @@ tinymce.PluginManager.add('wpgallery', function( editor ) {
 
 	function caption_shortcode( shortcode ) {
 		var content = wp.shortcode.replace('credit', shortcode.content, credit_shortcode);
-		console.log('caption content is ', content);
 		return '<figcaption class="wp-caption-text"><span class="media-caption">'
 		+ content + '</span></figcaption>';
 	}
 	function credit_shortcode( shortcode ) {
-		console.log('credit content is ', content);
 		return '</span><span class="media-credit">' + shortcode.content;
 	}
 
@@ -43,7 +84,6 @@ tinymce.PluginManager.add('wpgallery', function( editor ) {
 		var width = /([0-9]+)/.exec(figure.attr('style'))[1];
 		var img = figure.find('img').prop('outerHTML');
 		img = img ? img + '</img>' : '';
-		console.log('image is ', img);
 
 		var $credit = figure.find('span.media-credit'); 
 		var credit = $credit.html() 
@@ -53,14 +93,20 @@ tinymce.PluginManager.add('wpgallery', function( editor ) {
 		var caption = $caption.html() 
 			? '[caption]' + $caption.html() + credit + '[/caption]' 
 			: '[caption]' + credit + '[/caption]';
-		console.log('caption is ', caption);
 
-		console.log(img + caption);
-		console.log('[inline_image ' + id + ' align="' + align + '" width="' + width
-			+ '"]' + img + caption + '[/inline_image]');
 		return '[inline_image ' + id + ' align="' + align + '" width="' + width
 			+ '"]' + img + caption + '[/inline_image]';
 	}
+
+	function editMedia( node ) {
+
+		if ( node.nodeName !== 'IMG' ) {
+			return;
+		}
+
+		var image = dom.select( 'img[wp-media-selected]' )[0];
+	}
+
 
 	editor.on( 'BeforeSetContent', function( event ) {
 		event.content = replaceInlineImageShortcodes( event.content );
@@ -71,4 +117,104 @@ tinymce.PluginManager.add('wpgallery', function( editor ) {
 			event.content = restoreInlineImageShortcodes( event.content );
 		}
 	});
+
+	editor.on( 'ResolveName', function( event ) {
+		var dom = editor.dom,
+		node = event.target;
+
+		if ( node.nodeName === 'FIGURE' ) {
+			if ( dom.getAttrib( node, 'class' ).match('align') ) {
+				event.name = 'Image Container';
+			}
+		}
+		if ( node.nodeName === 'FIGCAPTION' ) {
+			if ( dom.hasClass( node, 'wp-caption-text' ) ) {
+				event.name = 'Image Data';
+			}
+		}
+		if ( node.nodeName === 'SPAN' && dom.hasClass( node, 'media-caption' ) ) {
+			event.name = 'Image Caption';
+		}
+		if ( node.nodeName === 'SPAN' && dom.hasClass( node, 'media-credit' ) ) {
+			event.name = 'Image Credit';
+		}
+	});
+	editor.on( 'cut', function() {
+		removeToolbar();
+	});
+
+
+	editor.addButton( 'mj_img_remove', {
+		tooltip: 'Remove',
+		icon: 'dashicon dashicons-no',
+		onclick: function() {
+			removeImage( editor.selection.getNode() );
+		}
+	} );
+
+	tinymce.each( {
+		alignleft: 'Align left',
+		aligncenter: 'Align center',
+		alignright: 'Align right',
+		alignnone: 'No alignment'
+	}, function( tooltip, name ) {
+		var direction = name.slice( 5 );
+
+		editor.addButton( 'mj_img_' + name, {
+			tooltip: tooltip,
+			icon: 'dashicon dashicons-align-' + direction,
+			onclick: function() {
+				console.log('ehre');
+				image_align(name, editor.selection.getNode());
+			},
+			onPostRender: function() {
+				var self = this;
+
+				editor.on( 'NodeChange', function( event ) {
+					var node;
+
+
+					node = editor.dom.getParent( event.element, '.inline-image' ) || event.element;
+
+					// Don't bother.
+					if ( node.nodeName !== 'FIGURE' ) {
+						return;
+					}
+
+					if ( 'alignnone' === name ) {
+						self.active( ! /\balign(left|center|right)\b/.test( node.className ) );
+					} else {
+						self.active( editor.dom.hasClass( node, name ) );
+					}
+				} );
+			}
+		} );
+	} );
+
+	editor.once( 'preinit', function() {
+		if ( editor.wp && editor.wp._createToolbar ) {
+			toolbar = editor.wp._createToolbar( [
+				'mj_img_alignleft',
+				'mj_img_aligncenter',
+				'mj_img_alignright',
+				'mj_img_alignnone',
+				'mj_img_remove'
+			] );
+		}
+	} );
+
+	editor.on( 'wptoolbar', function( event ) {
+		console.log(event.element)
+		var wrap = editor.dom.getParent( event.element, 'figure.inline-image' );
+		console.log(wrap);
+		if ( 	
+			(  	wrap
+				|| (event.element.nodeName === 'FIGURE' && editor.dom.hasClass( event.element.nodeName, 'inline-image' ) )
+			)
+			&& ! isPlaceholder( event.element ) 
+		) {
+			event.toolbar = toolbar;
+		}
+	} );
+
 });
